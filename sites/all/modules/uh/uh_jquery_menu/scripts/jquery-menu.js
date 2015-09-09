fcfc0315388d70457a9d8fe3341d8d83fe622ab3
roxannederuijter <roxanne@urban-heroes.nl>
2015-09-09 15:54:09 +0200

    first build

--- sites/all/modules/uh/uh_jquery_menu/scripts/jquery-menu.js
+++ sites/all/modules/uh/uh_jquery_menu/scripts/jquery-menu.js
@@ -13,6 +13,32 @@
             $('html,body').animate({scrollTop:$(this.hash).offset().top - 140}, 1000);
         });
     });
+
+    // Disable scroll zooming and bind back the click event
+    var onMapMouseleaveHandler = function (event) {
+        var that = $(this);
+
+        that.on('click', onMapClickHandler);
+        that.off('mouseleave', onMapMouseleaveHandler);
+        that.find('iframe').css("pointer-events", "none");
+    }
+
+    var onMapClickHandler = function (event) {
+        var that = $(this);
+
+        // Disable the click handler until the user leaves the map area
+        that.off('click', onMapClickHandler);
+
+        // Enable scrolling zoom
+        that.find('iframe').css("pointer-events", "auto");
+
+        // Handle the mouse leave event
+        that.on('mouseleave', onMapMouseleaveHandler);
+    }
+
+// Enable map zooming with mouse scroll when the user clicks the map
+    $('.maps.embed-container').on('click', onMapClickHandler);
+
 })(jQuery);
 
 
@@ -222,4 +248,6 @@
 
     });
 })(jQuery);
+
+
 
